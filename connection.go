cd07caeeb39ff47fcf171e83abcd07aa7223adea
wwqgtxx <wwqgtxx@gmail.com>
2024-05-21 00:00:10 +0800

    expose Config to Connection

--- connection.go
+++ connection.go
@@ -3122,3 +3122,8 @@ func (c *Conn) SetCongestionControl(cc congestion.CongestionControl) {
 func (c *Conn) SetRemoteAddr(addr net.Addr) {
 	c.conn.SetRemoteAddr(addr)
 }
+
+// Config Return current config
+func (c *Conn) Config() *Config {
+	return c.config
+}
